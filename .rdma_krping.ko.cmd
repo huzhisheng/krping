11a17802983aaa3f0a104834c93bb2c934db0a07
huzhisheng <1216265845@qq.com>
2022-08-19 12:39:43 +0800

    second commit

--- .rdma_krping.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/hzs/krping/rdma_krping.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/hzs/krping/rdma_krping.ko /home/hzs/krping/rdma_krping.o /home/hzs/krping/rdma_krping.mod.o ;  true
--- .rdma_krping.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/hzs/krping/rdma_krping.o := ld -m elf_x86_64   -r -o /home/hzs/krping/rdma_krping.o /home/hzs/krping/getopt.o /home/hzs/krping/krping.o 
